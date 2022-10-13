d07c5ced1ea00b1b3d87652a73cfe258f920bde1
nunusmohum <halojw301@gmail.com>
2022-10-13 12:35:02 +0900

    fisrt setting

---
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
@@ -0,0 +1,50 @@
+package com.ll.exam.app__2022_10_05.app.member.service;
+
+import com.ll.exam.app__2022_10_05.app.member.entity.Member;
+import com.ll.exam.app__2022_10_05.app.member.repository.MemberRepository;
+import com.ll.exam.app__2022_10_05.app.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final JwtProvider jwtProvider;
+
+    public Member join(String username, String password, String email) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
+
+        memberRepository.save(member);
+
+        return member;
+    }
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    @Transactional
+    public String genAccessToken(Member member) {
+        String accessToken = member.getAccessToken();
+
+        if (StringUtils.hasLength(accessToken) == false ) {
+            accessToken = jwtProvider.generateAccessToken(member.getAccessTokenClaims(), 60L * 60 * 24 * 365 * 100);
+            member.setAccessToken(accessToken);
+        }
+
+        return accessToken;
+    }
+
+    public boolean verifyWithWhiteList(Member member, String token) {
+        return member.getAccessToken().equals(token);
+    }
+}
